5f566edf08182a61e11a61fc5ab689e796e7fd64
William <rinconwilliam996@gmail.com>
2023-10-27 12:56:26 -0500

    despliegue

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,3 +1,3 @@
 <?php
-    $conexion = mysqli_connect("localhost", "root", "", "variedadesyoli");
+    $conexion = mysqli_connect("sql302.infinityfree.com", "if0_35316960", "tGlLTLlTx4Fy", "if0_35316960_variedadesyoli");
 ?>
