fdeee3e50de75355fe804dc04973dbab1d1496c0
William <rinconwilliam996@gmail.com>
2023-12-01 04:26:21 -0500

    base de datos

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion = mysqli_connect("smysql:host=localhost", "variedad_root", "william27valenata", "variedad_variedadesyoli");
+    //$conexion = mysqli_connect("smysql:host=localhost", "variedad_root", "william27valenata", "variedad_variedadesyoli");
     //$conexion = mysqli_connect("sql302.infinityfree.com", "if0_35316960", "tGlLTLlTx4Fy", "if0_35316960_variedadesyoli"); Conexion base de datos Web Hosting Infinity Free
-    //$conexion = mysqli_connect("localhost", "root", "william27valenata", "variedadesyoli"); //Conexion base de datos MySql Xampp
+    $conexion = mysqli_connect("localhost", "root", "william27valenata", "variedadesyoli"); //Conexion base de datos MySql Xampp
 ?>
